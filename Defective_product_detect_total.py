7b0d9d9ec94ed1c9d75cd922a2066ed4482440e0
J-Mook <74070059+J-Mook@users.noreply.github.com>
2020-11-08 01:03:58 +0900

    Update Defective_product_detect_total.py

--- Defective_product_detect_total.py
+++ Defective_product_detect_total.py
@@ -8,18 +8,16 @@ img_cp=img.copy()
 arr_cir=[0,0,0,0,0,0]
 arr_def=[0,0,0,0,0,0]
 arr_color = [0,0,0,0,0,0]
+arr_color_eng = [0,0,0,0,0,0]
 arr_updn = [0,0,0,0,0,0]
+
+colorset=[[0,160,50],[0,120,220],[150,30,150],[225,0,0],[255,100,0],[255,200,0]] #색 순서(g,b,p,r,o,y)
+colornum2eng = ['g','b','p','r','o','y']
 
 # resize
 ver=1000 #행 크기 (세로)
 hor=1000 #열 크기 (가로)
 img_re=cv2.resize(img_cp,(ver,hor))
-
-colorset=[[0,160,50],[0,120,220],[150,30,150],[225,0,0],[255,100,0],[255,200,0]] #색 순서(g,b,p,r,o,y)
-
-colornum2eng = ['g','b','p','r','o','y']
-(h,w) = img_re.shape[:2]
-(cX, cY) = (w / 2, h / 2)
 
 #흑백변환
 img_gray=cv2.cvtColor(img_re,cv2.COLOR_BGR2GRAY)
@@ -32,20 +30,16 @@ img_edge = cv2.addWeighted(img_edge_x, 1, img_edge_y, 1, 0)
 #원검출 전처리
 img_gray_cir = cv2.GaussianBlur(img_gray, (5,5), 0) #가우시안 블러
 img_edge_cir = cv2.Laplacian(img_gray_cir, cv2.CV_8U, ksize=5) #라플라시안 엣지
-(
-# circles = cv2.HoughCircles(img_edge_cir, cv2.HOUGH_GRADIENT, 1, 200, param1 = 180, param2 = 50, minRadius = 80, maxRadius = 108)
-# circles = np.uint16(np.around(circles))
-# for i in circles[0,:]:
-#     cv2.circle(img_re, (i[0], i[1]), i[2], (0, 255, 0), 5)
-)
+
+
 for num in range(0,6):
     colordistance = [0,0,0,0,0,0] #색거리
     colorchk = [0,0,0] #측정한 rgb값
 
-    M = cv2.getRotationMatrix2D((cX, cY), 60*num, 1.0)
-    rotated = cv2.warpAffine(img_re, M, (w, h))
-    rotated_def = cv2.warpAffine(img_edge, M, (w, h)) #불량검출 이미지 회전
-    rotated_cir = cv2.warpAffine(img_edge_cir, M, (w, h)) #원검출 이미지 회전
+    M = cv2.getRotationMatrix2D((hor/2, ver/2), 60*num, 1.0)
+    rotated = cv2.warpAffine(img_re, M, (hor, ver))
+    rotated_def = cv2.warpAffine(img_edge, M, (hor, ver)) #불량검출 이미지 회전
+    rotated_cir = cv2.warpAffine(img_edge_cir, M, (hor, ver)) #원검출 이미지 회전
     
     img_crop_def = rotated_def[269:310 , 425:575] #불량검출 이미지 크롭
     img_crop_cir = rotated_cir[:400 , 300:700] #원검출 이미지 크롭
@@ -64,14 +58,12 @@ for num in range(0,6):
         colordistance[i] = sqrt((colorset[i][0]-colorchk[0])**2+(colorset[i][1]-colorchk[1])**2+(colorset[i][2]-colorchk[2])**2)
         
         arr_color[num] = colordistance.index(min(colordistance)) #problem3 answer
-        # arr_color[num] = colornum2eng[colordistance.index(min(colordistance))] #이해하기 쉽게 변환
+        arr_color_eng[num] = colornum2eng[colordistance.index(min(colordistance))] #이해하기 쉽게 변환
 
     
     try:
         circles = cv2.HoughCircles(img_crop_cir, cv2.HOUGH_GRADIENT, 1, 200, param1 = 180, param2 = 50, minRadius = 80, maxRadius = 108)
         circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            cv2.circle(img_crop_cir, (i[0], i[1]), i[2], (0, 255, 0), 5)
     except:
         circles = 0 #원검출 안됬을경우 예외처리
 
@@ -86,9 +78,42 @@ for num in range(0,6):
 for num in range(0,6):
     if arr_def[num] == arr_cir[num]:
             arr_updn[num] = 1
-        
+            
+circles = cv2.HoughCircles(img_edge_cir, cv2.HOUGH_GRADIENT, 1, 200, param1 = 180, param2 = 50, minRadius = 80, maxRadius = 108)
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    cv2.circle(img_re, (i[0], i[1]), i[2], (255, 255, 255), 5)
+            
+# redius = 300
+# for i in range(0,6):
+    # cv2.putText(img_re,str(arr_color_eng[i]),(int(460+redius*sin(90-i*60)),int(510+redius*cos(90-i*60))),cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 3, cv2.LINE_AA)
 
-cv2.imshow('img', img)
+            
+cv2.putText(img_re,str(arr_color_eng[0]),(470,380),cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 3, cv2.LINE_AA)
+cv2.putText(img_re,str(arr_color_eng[1]),(610,420),cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 3, cv2.LINE_AA)
+cv2.putText(img_re,str(arr_color_eng[2]),(610,600),cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 3, cv2.LINE_AA)
+cv2.putText(img_re,str(arr_color_eng[3]),(470,680),cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 3, cv2.LINE_AA)
+cv2.putText(img_re,str(arr_color_eng[4]),(340,600),cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 3, cv2.LINE_AA)
+cv2.putText(img_re,str(arr_color_eng[5]),(340,420),cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,255), 3, cv2.LINE_AA)
+
+
+for num in range(0,6):
+    arr_cir[num] = str(arr_cir[num])
+    arr_updn[num] = str(arr_updn[num])
+    arr_color[num] = str(arr_color[num])
+    
+cv2.putText(img_re,str(''.join(arr_color)),(780,90),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3, cv2.LINE_AA)
+cv2.putText(img_re,str(''.join(arr_updn)),(780,130),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3, cv2.LINE_AA)
+cv2.putText(img_re,str(''.join(arr_cir)),(780,160),cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3, cv2.LINE_AA)
+
+
+
+cv2.imshow('img1', img)
+cv2.imshow('img2', img_re)
+cv2.imshow('img3', img_gray)
+cv2.imshow('img4', img_gray_cir)
+cv2.imshow('img5', img_edge)
+cv2.imshow('img6', img_edge_cir)
 # cv2.imshow('imgb', img_B)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
